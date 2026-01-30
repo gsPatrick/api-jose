e2426ab3c858b86ddd997100c884813bd6d4c5fa
Patrick Siqueira <patrickgsiqueira@hotmail.com>
2026-01-30 14:21:15 -0300

    Fix: Register pgvector type with Sequelize

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,18 @@
 const { Sequelize } = require('sequelize');
+const { registerType } = require('pgvector/sequelize');
+registerType(Sequelize);
 require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  logging: false, // Set to console.log to see SQL queries
+    dialect: 'postgres',
+    logging: false, // Set to console.log to see SQL queries
 });
 
 const connectDB = async () => {
     try {
         await sequelize.authenticate();
         console.log('PostgreSQL Connected via Sequelize');
-        
+
         // Ensure pgvector extension is enabled
         await sequelize.query('CREATE EXTENSION IF NOT EXISTS vector;');
         console.log('pgvector extension ensured.');
